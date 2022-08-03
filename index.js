015e20bf312370b2f5d454b6b56f5d5d8e0feebf
7Hassan <egoker1234@gmail.com>
2022-08-03 18:52:57 +0200

    add js file

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("icon-click").onclick=function(){
+    if (document.getElementById("box-nav").style.display=="block") {
+        document.getElementById("box-nav").style.display="none";
+    }else{
+        document.getElementById("box-nav").style.display="block";
+    }
+   
+}
